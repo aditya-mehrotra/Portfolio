4a0feba7b9e9343723672880b1b7a12315b877b4
Aditya <adityamehrotra6@gmail.com>
2025-07-26 12:55:10 +0530

    SpeedInsights Added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Aditya Mehrotra",
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth">
       <body className="select-none">
         <Analytics />
+        <SpeedInsights />
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
